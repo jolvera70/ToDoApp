cf042ec8103b0e73dd4d47c9cf42849449bce2e5
Jorge <jolvera70@hotmail.com>
2017-09-02 01:07:33 -0500

    insert and read from db

--- app/src/main/java/android/arbol/com/todoapp/main/AddObjectActivity.java
+++ app/src/main/java/android/arbol/com/todoapp/main/AddObjectActivity.java
@@ -3,6 +3,10 @@ package android.arbol.com.todoapp.main;
 import android.arbol.com.todoapp.R;
 import android.arbol.com.todoapp.auth.BaseActivity;
 import android.arbol.com.todoapp.auth.ChooserActivity;
+import android.arbol.com.todoapp.main.entities.GroupCare;
+import android.arbol.com.todoapp.main.entities.MediaCare;
+import android.arbol.com.todoapp.main.entities.ObjectCare;
+import android.arbol.com.todoapp.main.entities.UserCare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,15 +22,22 @@ import android.widget.TextView;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import android.arbol.com.todoapp.main.entities.AddressObjectCare;
+
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Main5Activity extends BaseActivity {
+
+public class AddObjectActivity extends BaseActivity {
 
     private RecyclerView mRecyclerView;
     private StorageReference mStorageRef;
@@ -35,14 +47,14 @@ public class Main5Activity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5);
+        setContentView(R.layout.activiry_add_object);
 
         final Button buttonId = (Button) findViewById(R.id.button_id);
         buttonId.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 LoginManager.getInstance().logOut();
-                startActivity(new Intent(Main5Activity.this, ChooserActivity.class));
+                startActivity(new Intent(AddObjectActivity.this, ChooserActivity.class));
             }
         });
 
@@ -50,16 +62,19 @@ public class Main5Activity extends BaseActivity {
         final TextView userId = (TextView) findViewById(R.id.userId);
         userId.append(" " + currentUser.getDisplayName());
 
-        final ListView listView = (ListView) findViewById(R.id.listView);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("appArbol_ObjectCare");
+
+        final ListView listView = (ListView) findViewById(R.id.listViewArbol);
         // Create a new Adapter
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1);
+        final ArrayAdapter<Map<String,String>> adapter = new ArrayAdapter<Map<String,String>>(this,
+                android.R.layout.simple_list_item_1);
         // Assign adapter to ListView
         listView.setAdapter(adapter);
         // Connect to the Firebase database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
         // Get a reference to the todoItems child items it the database
-        final DatabaseReference myRef = database.getReference("todoItems");
+
         // Assign a listener to detect changes to the child items
         // of the database reference.
         myRef.addChildEventListener(new ChildEventListener() {
@@ -69,13 +84,13 @@ public class Main5Activity extends BaseActivity {
             // each time a new child is added.
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
-                String value = dataSnapshot.getValue(String.class);
-                adapter.add(value);
+                Map<String, String> map = (Map<String, String>) dataSnapshot.getValue();
+                adapter.add(map);
             }
 
             // This function is called each time a child item is removed.
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
+                HashMap<String,String> value = dataSnapshot.getValue(HashMap.class);
                 adapter.remove(value);
             }
 
@@ -94,23 +109,30 @@ public class Main5Activity extends BaseActivity {
         });
 
 
-
         // Add items via the Button and EditText at the bottom of the window.
-        /*final EditText text = (EditText) findViewById(R.id.todoText);
+        final EditText text = (EditText) findViewById(R.id.todoText);
         final Button button = (Button) findViewById(R.id.addButton);
-
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                final DatabaseReference myRef = database.getReference("appArbol_ObjectCare");
+                String uid = myRef.child("objects").push().getKey();
 
-                // Create a new child with a auto-generated ID.
-                DatabaseReference childRef = myRef.push();
+                Map<String, ObjectCare> objectCareMap = createObjectCare(uid);
+                myRef.child("objects").setValue(objectCareMap);
 
-                // Set the child's data to the value passed in from the text box.
-                childRef.setValue(text.getText().toString());
+                Map<String, GroupCare> groupCareMap = createGroup(uid);
+                myRef.child("groups").setValue(groupCareMap);
 
+                Map<String, MediaCare> imageCareMap = createImageGroup(uid);
+                myRef.child("images").setValue(imageCareMap);
+
+                Map<String, UserCare> userCareMap = createUserCare(uid);
+                myRef.child("user").setValue(userCareMap);
+                finish();
             }
         });
-*/
+
         // Delete items when clicked
   /*      listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -160,4 +182,85 @@ public class Main5Activity extends BaseActivity {
             });
     }*/
     }
+
+    private  Map<String, ObjectCare> createObjectCare(String uid) {
+        ObjectCare objectCare = new ObjectCare();
+        Map<String, ObjectCare> objectCareMap = new HashMap<String, ObjectCare>();
+        Map<String,Boolean> groups = new HashMap<String, Boolean>();
+        Map<String,Boolean> images = new HashMap<String, Boolean>();
+
+        AddressObjectCare address = new AddressObjectCare();
+        address.setLatitude(25.686613);
+        address.setLongitude(-100.316116);
+
+        objectCare.setUid(uid);
+        objectCare.setName("Arbol 1");
+        images.put("imagesObject"+objectCare.getUid(),true);
+        objectCare.setImages(images);
+        groups.put("groupCuidadores"+objectCare.getUid(),true);
+        groups.put("groupPagadores"+objectCare.getUid(),true);
+        objectCare.setGroups(groups);
+        objectCare.setAddress(address);
+
+        objectCareMap.put(objectCare.getUid(),objectCare);
+
+        objectCareMap.put(uid,objectCare);
+        return objectCareMap;
+    }
+
+    private  Map<String, GroupCare> createGroup(String uid) {
+        Map<String,Boolean> object = new HashMap<String, Boolean>();
+        Map<String,Map<String,String>> pagos = new HashMap<String,Map<String,String>>();
+        Map<String,Map<String,String>> cobros = new HashMap<String,Map<String,String>> ();
+        object.put(uid,true);
+        GroupCare groupCare = new GroupCare();
+        groupCare.setNameGroup("groupCuidadores"+uid);
+        groupCare.setObjects(object);
+        Map<String,String> datosPago = new HashMap<String,String>();
+        Map<String,String> datosPago2 = new HashMap<String,String>();
+        datosPago.put("monto","$100");
+        datosPago.put("date","02/09/2017");
+        datosPago.put("type","paypal");
+        pagos.put("0",datosPago);
+        datosPago2.put("monto","$200");
+        datosPago2.put("date","01/09/2017");
+        datosPago2.put("type","tdc");
+        pagos.put("1",datosPago2);
+        groupCare.setPagos(pagos);
+        groupCare.setCobros(cobros);
+
+        Map<String, GroupCare> groupCareMap = new HashMap<String, GroupCare>();
+        groupCareMap.put("groupCuidadores"+uid,groupCare);
+        return groupCareMap;
+    }
+
+    private  Map<String, MediaCare> createImageGroup(String uid) {
+        Map<String,Boolean> object = new HashMap<String, Boolean>();
+        object.put(uid,true);
+        MediaCare mediaCare = new MediaCare();
+        mediaCare.setMediaName("imagesObject"+uid);
+        Map<String,String> images = new HashMap<String, String>();
+        images.put(uid,"image1.gif");
+        images.put("uid2","image2.gif");
+        mediaCare.setImages(images);
+        mediaCare.setObjects(object);
+
+        Map<String, MediaCare> imageCareMap = new HashMap<String, MediaCare>();
+        imageCareMap.put("imagesObject"+uid, mediaCare);
+        return imageCareMap;
+    }
+
+    private  Map<String, UserCare> createUserCare(String uid) {
+        Map<String,Boolean> groups = new HashMap<String, Boolean>();
+        groups.put("groupCuidadores"+uid,true);
+        UserCare userCare = new UserCare();
+        final EditText text = (EditText) findViewById(R.id.todoText);
+        userCare.setUserName(text.getText().toString());
+        userCare.setGroups(groups);
+
+        Map<String, UserCare> userCareMap = new HashMap<String, UserCare>();
+        userCareMap.put("user"+uid, userCare);
+        return userCareMap;
+    }
 }
+
